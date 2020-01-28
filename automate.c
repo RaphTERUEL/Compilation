72f7d0d6b3d403a825cf0eb1936a334381b069d0
raph <r.teruel997@gmail.com>
2020-01-28 14:58:04 +0100

    balek

--- automate.c
+++ automate.c
@@ -30,13 +30,18 @@ void affichage(automate autoFiniNonDeter);
 
 int main(int nbarg, char *args[])
 {
+    printf("sfldjh\n");
 	automate autoFiniNonDeter;
 	autoFiniNonDeter.nbr_etats=1;
-	vertex v;
-	v.numSommet=0;
-	v.nb_edge=0;
-	autoFiniNonDeter.nextVertex=&v;
-	addVertex(&v, &autoFiniNonDeter, 'a');
+	autoFiniNonDeter.nextVertex=NULL;
+	vertex *v=malloc(sizeof(vertex));
+
+    v->numSommet=0;
+	v->nb_edge=0;
+
+	autoFiniNonDeter.nextVertex=v;
+
+	addVertex(v, &autoFiniNonDeter, 'a');
 	affichage(autoFiniNonDeter);
 
 
@@ -44,29 +49,31 @@ int main(int nbarg, char *args[])
 }
 
 void addVertex(vertex *vert, automate *autoFiniNonDeter, char carac) {
-	vertex v;
-	edge E;
-	v.nb_edge=0;
-	v.tab_edge=NULL;
-	v.numSommet=autoFiniNonDeter->nbr_etats;
-	autoFiniNonDeter->nbr_etats++;
+	vertex *v=malloc(sizeof(vertex));
+	edge*  E=malloc(sizeof(edge));
+	v->nb_edge=0;
+	v->tab_edge=NULL;
+	v->numSommet=autoFiniNonDeter->nbr_etats;
+	autoFiniNonDeter->nbr_etats+=1;
 	vert->nb_edge++;
 	vert->tab_edge=(edge**)malloc(sizeof(edge*)*vert->nb_edge);
-	vert->tab_edge[vert->nb_edge-1]=&E;
-	E.nextVertex=&v;
-	E.word=carac;
+	vert->tab_edge[vert->nb_edge-1]=E;
+	E->nextVertex=v;
+	E->word=carac;
+
 
 }
 
 void affichage(automate autoFiniNonDeter){
-	vertex v;
+	vertex *v=malloc(sizeof(vertex));
 	int i;
-	v=*autoFiniNonDeter.nextVertex;
-	while(v.tab_edge!=NULL){
-		for (i = 0; i< v.nb_edge; i++){
-			printf("%d --> %c --> %d\n",v.numSommet,v.tab_edge[i]->word,v.tab_edge[i]->nextVertex->numSommet);
+	v=autoFiniNonDeter.nextVertex;
+	while (v->tab_edge!=NULL){
+	    printf("lol\n");
+		for (i = 0; i< v->nb_edge; i++){
+			printf("%d --> %c --> %d\n",v->numSommet,v->tab_edge[i]->word,v->tab_edge[i]->nextVertex->numSommet);
 		}
-		v=*v.tab_edge[i-1]->nextVertex;
+		v=v->tab_edge[0]->nextVertex;
 	}
 
 }
