6a8989b684160956e8719b079eed9edf4af5cdf8
broumec <r.bryan98@hotmail.fr>
2020-02-10 21:07:19 +0100

    Avec .h

---
+++ automate.h
@@ -0,0 +1,78 @@
+#ifndef AUTOMATE_H
+#define AUTOMATE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef enum {FALSE, TRUE}bool;
+#define couleur(param) printf("\033[%sm",param)
+
+typedef struct vertex vertex;
+typedef struct edge edge;
+typedef struct automate automate;
+
+struct edge{
+	vertex *nextVertex;
+	char word;
+};
+
+struct vertex{
+	int numSommet;
+	edge *tab_edge;
+	int nb_edge;
+	bool accepteur;
+};
+
+struct automate{
+	vertex *Vertex;
+	int nbr_etats;
+};
+
+
+
+
+typedef struct Trans
+{
+    int *sommet;
+    int size;
+    char carra;
+    struct Trans *next;
+}Trans;
+
+typedef struct Enslist
+{
+    int *sommet;
+    int size;
+    struct Enslist *next;
+    Trans * First;
+    int nb_T;
+}Enslist;
+
+typedef struct
+{
+    Enslist *first;
+    int nb;
+}List;
+
+
+
+
+automate * reuninon(automate * a, automate * b);
+automate * concatenation(automate * a, automate * b);
+void addEdge(vertex *a,vertex * b, char carac) ;
+void affichage(automate autoFiniNonDeter);
+automate* motVide();
+automate * langagevide(void);
+automate* caracter(char *c);
+automate * kleen(automate * a);
+Enslist * ajouter_Trans(Enslist * e,char car,int voisin);
+int vExiste(List * l, int* e, int nb);
+Enslist * Derniermayon(List * list);
+automate * Determinisation(automate * a);
+int charintab(char*car,char c,int nb);
+char *transitiondiferent(automate* a, char *car);
+int trensitionexiste(vertex*v, char c);
+bool tableauegale(int * a, int * b,int nb);
+automate * minimisation(automate *a);
+int nbtrensition(automate* a,char *car);
+
+#endif
